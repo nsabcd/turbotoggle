414caf2c0095a95925322e36ceb02443bec09fb9
Nihar Sharma <niharsharma@Nihars-MacBook-Air.local>
2026-07-25 10:05:32 +0530

    chore(config): initialize project skeleton, gradle wrapper, multi-profile YAMLs, and docker-compose

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "turbotoggle-server"
---
+++ src/test/java/com/turbotoggle/TurbotoggleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turbotoggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbotoggleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
